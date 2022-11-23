92d205a04309884b1442c72d6b24f58e0caee721
bmdevops <bangarumounikareddy@gmail.com>
2022-11-23 15:11:13 +0530

    updated

--- one.java
+++ one.java
@@ -2,5 +2,5 @@ class ab
 {
 public static void main (String [] args) {
 	System.Out.println("Hello world");
-        System.Out.println("my name is surya"); 
+        System.Out.println("================"); 
 }}
